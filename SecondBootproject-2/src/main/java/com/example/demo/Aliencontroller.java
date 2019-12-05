a0f799599dc7d5871dea01edde1cc3b440b0d88f
bharathadpad97 <58290681+bharathadpad97@users.noreply.github.com>
2019-12-05 17:53:08 +0530

    SpringBoot-2

---
+++ SecondBootproject-2/src/main/java/com/example/demo/Aliencontroller.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+
+import java.io.FileInputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.Alienclass;
+import com.sun.xml.fastinfoset.sax.Properties;
+
+@Controller
+public class Aliencontroller {
+
+	Alienclass alien=new Alienclass();
+	@Autowired
+	Repository repo;
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		return "firstpage";
+	}
+	
+	
+
+	@RequestMapping (value="/aliendataadd")
+	public String controllermethod(@RequestParam int id,@RequestParam String name)
+	{
+		
+		System.out.println(alien.getAid());
+		alien.getAname();
+		alien.getAtech();
+		repo.save(alien);
+		System.out.println("value to check" +alien.getAid());
+		return "firstpage";
+		
+	}
+
+}
---
+++ SecondBootproject-2/src/main/webapp/pages/firstpage.jsp
@@ -0,0 +1,28 @@
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<p> Enter Username and Password </p>
+<FORM action="aliendataadd" >
+    <P>
+    <LABEL for="Userid">Userid </LABEL>
+              <INPUT type="text" id="Userid"><BR>
+    <LABEL for="Username">Username </LABEL>
+              <INPUT type="text" id="Username"><BR>
+    <LABEL for="Technology">Usertech </LABEL>
+              <INPUT type="text" id="Usertech"><BR>
+        <INPUT type="submit">
+    </P>
+ </FORM>
+ <br>
+ <br>
+  
+ 
+</body>
+</html>
